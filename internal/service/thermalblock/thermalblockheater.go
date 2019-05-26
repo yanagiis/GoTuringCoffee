1c0ba46faa384a149ac9ee66a8af5bee2ed216c2
JimmyOu <yanagiis@gmail.com>
2019-05-26 19:04:02 +0800

    feat: add thermal block sensor and controller

---
+++ internal/service/thermalblock/thermalblockheater.go
@@ -0,0 +1,160 @@
+package thermalblockheater
+
+import (
+	"context"
+	"errors"
+	"math"
+	"time"
+
+	"GoTuringCoffee/internal/hardware"
+	"GoTuringCoffee/internal/service/lib"
+	"GoTuringCoffee/internal/service/thermalblocktemp"
+
+	nats "github.com/nats-io/go-nats"
+	"github.com/rs/zerolog/log"
+)
+
+type Service struct {
+	ScanInterval   time.Duration
+	pwm            hardware.PWM
+	pid            lib.PID
+	sensorErr      error
+	record         lib.HeaterRecord
+	lastTempRecord *lib.TempRecord
+}
+
+func NewService(dev hardware.PWM, scanInterval time.Duration, pid lib.PID) *Service {
+	return &Service{
+		ScanInterval: scanInterval,
+		pwm:          dev,
+		pid:          pid,
+	}
+}
+
+func (h *Service) Run(ctx context.Context, nc *nats.EncodedConn, fin chan<- struct{}) (err error) {
+	nc.Subscribe("thermalblock.heater", func(subj, reply string, req lib.HeaterRequest) {
+		if req.IsGet() {
+			resp := h.handleHeaterStatus()
+			nc.Publish(reply, resp)
+		}
+		if req.IsPut() {
+			resp := h.handleSetTemperature(req.Temp)
+			nc.Publish(reply, resp)
+		}
+	})
+
+	if err = h.pwm.Connect(); err != nil {
+		log.Info().Msg("Heater device connect failed")
+		return
+	}
+
+	h.record.Target = 90
+	h.pid.SetBound(0, 100)
+	h.pid.SetPoint(h.record.Target)
+
+	timer := time.NewTimer(h.ScanInterval)
+	for {
+		select {
+		case <-timer.C:
+			h.adjustTemperature(ctx, nc)
+			timer = time.NewTimer(h.ScanInterval)
+		case <-ctx.Done():
+			log.Info().Msg("stoping heater service")
+			h.pwm.Disconnect()
+			err = ctx.Err()
+			defer func() { fin <- struct{}{} }()
+			log.Info().Msg("stop heater service")
+			return
+		}
+	}
+
+}
+
+func (h *Service) handleHeaterStatus() lib.HeaterResponse {
+	var resp lib.HeaterResponse
+	if h.sensorErr != nil {
+		resp = lib.HeaterResponse{
+			Response: lib.Response{
+				Code: lib.CodeFailure,
+				Msg:  h.sensorErr.Error(),
+			},
+		}
+	} else {
+		resp = lib.HeaterResponse{
+			Response: lib.Response{
+				Code: lib.CodeSuccess,
+			},
+			Payload: h.record,
+		}
+	}
+	return resp
+}
+
+func (h *Service) handleSetTemperature(temp float64) lib.HeaterResponse {
+	log.Info().Msg("Set temperature")
+	h.record.Target = temp
+	h.pid.SetPoint(h.record.Target)
+	return lib.HeaterResponse{
+		Response: lib.Response{
+			Code: lib.CodeSuccess,
+		},
+	}
+}
+
+func (h *Service) adjustTemperature(ctx context.Context, nc *nats.EncodedConn) error {
+	resp, err := thermalblocktemp.GetTemperature(ctx, nc)
+	if err != nil {
+		h.pwm.PWM(0, 0)
+		return err
+	}
+	if resp.IsFailure() {
+		h.pwm.PWM(0, 0)
+		return errors.New("Cannot get thermalblock temperature")
+	}
+	if math.IsNaN(resp.Payload.Temp) {
+		h.pwm.PWM(0, 0)
+		return errors.New("Cannot get NaN temperature")
+	}
+	if resp.Payload.Temp <= 0 {
+		h.pwm.PWM(0, 0)
+		return errors.New("Cannot get negative temperature")
+	}
+	difftime := time.Second * 0
+	if h.lastTempRecord != nil {
+		difftime = resp.Payload.Time.Sub(h.lastTempRecord.Time)
+	}
+	duty := h.pid.Compute(resp.Payload.Temp, difftime) / 100
+	if err := h.pwm.PWM(duty, 100000); err != nil {
+		log.Error().Msg(err.Error())
+		return err
+	}
+	h.record.Duty = duty
+	h.record.Time = time.Now()
+	h.lastTempRecord = &resp.Payload
+	return nil
+}
+
+func (h *Service) Stop() error {
+	return nil
+}
+
+func GetHeaterInfo(ctx context.Context, nc *nats.EncodedConn) (resp lib.HeaterResponse, err error) {
+	req := lib.HeaterRequest{
+		Request: lib.Request{
+			Code: lib.CodeGet,
+		},
+	}
+	err = nc.RequestWithContext(ctx, "thermalblock.heater", &req, &resp)
+	return
+}
+
+func SetTemperature(ctx context.Context, nc *nats.EncodedConn, temp float64) (resp lib.HeaterResponse, err error) {
+	req := lib.HeaterRequest{
+		Request: lib.Request{
+			Code: lib.CodePut,
+		},
+		Temp: temp,
+	}
+	err = nc.RequestWithContext(ctx, "thermalblock.heater", &req, &resp)
+	return
+}
---
+++ internal/service/thermalblock/thermalblocktemp.go
@@ -0,0 +1,95 @@
+package thermalblocktemp
+
+import (
+	"context"
+	"math"
+	"time"
+
+	"GoTuringCoffee/internal/hardware"
+	"GoTuringCoffee/internal/service/lib"
+
+	nats "github.com/nats-io/go-nats"
+	"github.com/rs/zerolog/log"
+)
+
+type Service struct {
+	ScanInterval time.Duration
+	Sensor       hardware.TemperatureSensor
+}
+
+func NewService(dev hardware.TemperatureSensor, scanInterval time.Duration) *Service {
+	return &Service{
+		ScanInterval: scanInterval,
+		Sensor:       dev,
+	}
+}
+
+func (t *Service) Run(ctx context.Context, nc *nats.EncodedConn, fin chan<- struct{}) (err error) {
+	var sensorErr error
+	temperature := lib.TempRecord{
+		Temp: math.NaN(),
+		Time: time.Time{},
+	}
+
+	nc.Subscribe("thermalblock.temperature", func(subj, reply string, req lib.Request) {
+		var resp lib.TempResponse
+		if sensorErr != nil {
+			resp = lib.TempResponse{
+				Response: lib.Response{
+					Code: lib.CodeFailure,
+					Msg:  sensorErr.Error(),
+				},
+			}
+		} else {
+			resp = lib.TempResponse{
+				Response: lib.Response{
+					Code: lib.CodeSuccess,
+				},
+				Payload: temperature,
+			}
+		}
+		nc.Publish(reply, resp)
+	})
+
+	timer := time.NewTimer(t.ScanInterval)
+
+	for {
+		select {
+		case <-timer.C:
+			if sensorErr = t.Sensor.Connect(); sensorErr != nil {
+				log.Error().Msg(sensorErr.Error())
+				timer = time.NewTimer(t.ScanInterval)
+				continue
+			}
+			if temperature.Temp, sensorErr = t.Sensor.GetTemperature(); err != nil {
+				t.Sensor.Disconnect()
+				log.Error().Msg(sensorErr.Error())
+				timer = time.NewTimer(t.ScanInterval)
+				continue
+			}
+			temperature.Time = time.Now()
+			timer = time.NewTimer(t.ScanInterval)
+		case <-ctx.Done():
+			log.Info().Msg("stoping thermalblock temperature service")
+			err = ctx.Err()
+			defer func() { fin <- struct{}{} }()
+			log.Info().Msg("stop thermalblock temperature service")
+			return
+		}
+	}
+}
+
+func (t *Service) Stop() error {
+	return nil
+}
+
+func GetTemperature(ctx context.Context, nc *nats.EncodedConn) (resp lib.TempResponse, err error) {
+	req := lib.Request{
+		Code: lib.CodeGet,
+	}
+	err = nc.RequestWithContext(ctx, "thermalblock.temperature", &req, &resp)
+	if err != nil {
+		log.Error().Err(err).Msg("Get thermalblock temperature failed")
+	}
+	return
+}
